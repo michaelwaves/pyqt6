0233a606a8e743859c46d86b5286ac77325f1dd1
michaelwaves <michaelyu713705@gmail.com>
2023-11-17 10:41:23 +0800

    app improvements

--- app.py
+++ app.py
@@ -1,17 +1,19 @@
 from PyQt6.QtWidgets import QApplication, QVBoxLayout, QMainWindow, QPushButton, QLabel, QLineEdit, QToolTip, QWidget
+from PyQt6 import QtGui
 
 import sys
 import pandas as pd
 from math import isnan
 
 
-def fill_blanks(FILENAME):
+def fill_blanks(FILENAME, OUTPUT_FILENAME):
     df = pd.read_excel(FILENAME)
-    dates = df['Date'].tolist()
-    fps = df['FP'].tolist()
-    fys = df['FY'].tolist()
-    voucher_categories = df['Voucher Category'].tolist()
-    voucher_no = df['Voucher No.'].tolist()
+    columns = df.columns
+    dates = df[columns[0]].tolist()
+    fps = df[columns[1]].tolist()
+    fys = df[columns[2]].tolist()
+    voucher_categories = df[columns[3]].tolist()
+    voucher_no = df[columns[4]].tolist()
 
     def fill_blanks(list):
         current_element = list
@@ -32,13 +34,17 @@ def fill_blanks(FILENAME):
     new_voucher_categories = fill_blanks(voucher_categories)
     new_voucher_no = fill_blanks(voucher_no)
 
-    df['Date'] = new_dates
-    df['FP'] = new_fps
-    df['FY'] = new_fys
-    df['Voucher Category'] = new_voucher_categories
-    df['Voucher No.'] = new_voucher_no
+    df[columns[0]] = new_dates
+    df[columns[1]] = new_fps
+    df[columns[2]] = new_fys
+    df[columns[3]] = new_voucher_categories
+    df[columns[4]] = new_voucher_no
 
-    df.to_excel('output.xlsx')
+    try:
+        df.to_excel(OUTPUT_FILENAME, index=False)
+    except:
+        print("Please ensure there is a file extension like .xlsx in the output file name.")
+        print("Also, make sure the output file is not open, or the same name as the input file")
 
 
 class MainWindow(QMainWindow):
@@ -46,15 +52,35 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("Kingdee Export Formatting Tool")
         self.username = ""
+        self.output_filename = ""
+
+        self.title = QLabel("Input File Name (with extension like .xlsx):")
+        self.output_filename_title = QLabel(
+            "Output File Name (with extension like .xlsx):")
 
         self.label = QLabel()
         self.input = QLineEdit()
         self.input.textChanged.connect(self.label.setText)
         self.input.textChanged.connect(self.on_text_change)
 
+        self.output_filename_label = QLabel()
+        self.output_filename_input = QLineEdit()
+        self.output_filename_input.textChanged.connect(
+            self.output_filename_label.setText)
+        self.output_filename_input.textChanged.connect(
+            self.on_output_filename_change)
+
         layout = QVBoxLayout()
+
+        # input file
+        layout.addWidget(self.title)
         layout.addWidget(self.input)
         layout.addWidget(self.label)
+
+        # output file
+        layout.addWidget(self.output_filename_title)
+        layout.addWidget(self.output_filename_input)
+        layout.addWidget(self.output_filename_label)
 
         self.button = QPushButton("Fill Blanks")
         self.button.setFixedSize(150, 40)
@@ -70,16 +96,22 @@ class MainWindow(QMainWindow):
 
     def on_button_clicked(self):
         print("boom", self.username)
-        fill_blanks(self.username)
+        fill_blanks(self.username, self.output_filename)
 
     def on_text_change(self):
         self.username = self.input.text()
         print(self.username)
 
+    def on_output_filename_change(self):
+        self.output_filename = self.output_filename_input.text()
+        print(self.output_filename)
+
 
 app = QApplication([])
 
+
 window = MainWindow()
+window.setWindowIcon(QtGui.QIcon('kingdee.ico'))
 window.show()
 
 app.exec()
